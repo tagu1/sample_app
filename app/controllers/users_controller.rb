53ca9d96a4cdba6a6791929ad8cb94d97eae2986
Yoichi Taguchi <yo.taguchi@wonder.ocn.ne.jp>
2013-08-17 12:49:36 +0900

    temp 9 chapter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,17 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # 更新に成功した場合を扱う。
+    else
+      render 'edit'
+    end
+  end
 end
