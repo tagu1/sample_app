15c669cc200a58b8db452ac6ba3110974bd55542
Yoichi Taguchi <yo.taguchi@wonder.ocn.ne.jp>
2013-08-29 18:20:51 +0900

    Add user following chapter11

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,9 +32,7 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   def feed
-    # このコードは準備段階です。
-    # 完全な実装は第11章「ユーザーをフォローする」を参照してください。
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
 
   def following?(other_user)
